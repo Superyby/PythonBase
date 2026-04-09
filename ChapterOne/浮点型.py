dfaf55519673836ae122ffe01478bfc75bfdbd61
Tomato <1551405455@qq.com>
2026-04-09 08:28:42 +0800

    浮点型

---
+++ ChapterOne/浮点型.py
@@ -0,0 +1,22 @@
+# 浮点型就是带有小数的数字
+weight = 15.1
+height = 8.7
+price = 100.0
+
+print(price)
+
+# 科学计数法表示
+speed_of_sound = 3.14e+2 # 3.14乘10的2次方
+print(speed_of_sound)
+people_number = 7.8e9 # 7.8乘10的9次方
+print(people_number)
+distance_sun_earth = 1.496E8 # 1.496乘10的8次方
+print(distance_sun_earth)
+speed_of_light = 2.998E+8 # 2.998乘10的8次方
+print(speed_of_light)
+
+
+one_ml = 1e-3 # 1的10的-3次方
+print(one_ml)
+one_mg = 1E-3 # 1的10的-3次方
+print(one_mg)
