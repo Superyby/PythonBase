8ac4e5bdf344ecfa2a9ccc8afa8bab7da5f06b67
Tomato <1551405455@qq.com>
2026-04-13 07:12:57 +0800

    赋值_复合运算符

---
+++ ChapterOne/赋值_复合运算符.py
@@ -0,0 +1,38 @@
+age = 15  # 赋值运算符
+
+age += 1  # 加法赋值运算符(复合)
+
+# 减法复合运算符
+age1 = 15
+age1 -= 1
+print(age1)
+
+# 乘法赋值运算符
+count = 0.5
+sum = 100
+sum *= count
+print(sum)
+
+# 除法复合运算符
+pay = 100
+num1 = 5
+pay /= num1
+print(pay)
+
+# 取整赋值运算符
+apple = 31
+num2 = 14
+apple //= num2
+print(apple)
+
+# 取模赋值运算符
+ok = 10
+num3 = 3
+ok %= num3
+print(ok)
+
+# 指数赋值运算
+a = 2
+b = 3
+a **= b
+print(a)
