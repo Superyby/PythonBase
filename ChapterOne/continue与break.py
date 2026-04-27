befb631bf4c28698c399a6a021d74e8834b5c253
Tomato <1551405455@qq.com>
2026-04-27 21:52:18 +0800

    continue与break

--- ChapterOne/continue与break.py
+++ ChapterOne/continue与break.py
@@ -9,10 +9,24 @@ for day in range(1, 5):
     print('你好')
 
 for day in range(1, 5):
+    if day == 2:
+        break
+    print('你好')
+
+for day in range(1, 5):
     print(f'第{day}天')
     print('吃饭')
     for item in range(1, 3):
         print('面包')
         continue
+        print('banana')
+    print('ok')
+
+for day in range(1, 5):
+    print(f'第{day}天')
+    print('吃饭')
+    for item in range(1, 3):
+        print('面包')
+        break
         print('banana')
     print('ok')
