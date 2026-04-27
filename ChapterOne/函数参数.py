00d74a9354209e456990453b7035a993f35febb1
Tomato <1551405455@qq.com>
2026-04-27 22:03:55 +0800

    函数基本

--- ChapterOne/函数参数.py
+++ ChapterOne/函数参数.py
@@ -1,7 +1,13 @@
 def order(num, dish):
     print(f'您:{num}份，{dish}')
 
+
 order(1, '辣椒炒肉')
 
 
+#################### 位置参数 #########################
+def greet(name, height, age, gender):
+    print(f'我是{name},身高:{height},性别:{gender},年龄{age}')
 
+
+greet('张三', 150, 15, '男')
