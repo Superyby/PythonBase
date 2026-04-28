dbf7d0136b47abe48a986d4672394ec58784d3ff
Tomato <1551405455@qq.com>
2026-04-29 07:32:12 +0800

    list

---
+++ ChapterOne/datastructure/list/list.py
@@ -0,0 +1,12 @@
+list1 = [34, 25, 11, 10.5]
+list2 = ['python', 'java', 'c++']
+list3 = [1, 2, 3, 'python', 'java']
+list4 = [1, 2, 3, [4, 5, 6], 7]
+list5 = list()  # 创建一个空列表
+list6 = []
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+print(list6)
